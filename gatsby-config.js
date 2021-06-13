653a12f59251680131126ed6327e4e35c7884eb8
Mike Wong <mikewong0902@gmail.com>
2021-06-13 21:52:03 +0800

    config for deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Strata by HTML5 UP',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js Starter based on Strata by HTML5 UP',
+    title: 'Massie Wu Portfolio',
+    author: 'Mike Wong',
+    description: 'A Gatsby.js Portfolio',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -21,4 +21,6 @@ module.exports = {
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
   ],
+
+  pathPrefix: '/MassieWu_Portfolio',
 }
